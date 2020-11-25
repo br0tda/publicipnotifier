a86250e89a14e60c7d35f59dc23523026e728878
br0tda <45710698+br0tda@users.noreply.github.com>
2020-11-25 09:45:42 +0100

    Update IPext.py
    Added try/execpt to improve robustness.

--- IPext.py
+++ IPext.py
@@ -3,39 +3,54 @@ import requests
 import smtplib
 import time
 
+url = 'http://myexternalip.com/raw'
+fromaddr = 'sample@email.com'
+toaddrs  = 'samplereciver@mail.it'
+username = 'samplesender@email.com'
+password = 'my_password'
+server = smtplib.SMTP('smtp.sample.com:587')
+
 while 1:
 
-        in_file = open("IPextLog.txt","r")
-        lastIP = in_file.read()
-        in_file.close()
+        try:
+            in_file = open("IPextLog.txt","r")
+            lastIP = in_file.read()
+            in_file.close()
+        except:
+            print("FILE error")
         
-        url = 'http://myexternalip.com/raw'
-        response = requests.get(url)
-        IPext0 = BeautifulSoup(response.text, "html.parser")
+        try:
+            response = requests.get(url)
+            IPext0 = BeautifulSoup(response.text, "html.parser")
+            IPext= str(IPext0)
 
-        IPext= str(IPext0)
-        
-        if(IPext!=lastIP):
-            out_file = open("IPextLog.txt","w")
-            out_file.write(IPext)
-            out_file.close()
-            print("IP has changed\r\n")
-            print(IPext)
-            fromaddr = 'sample@email.com'
-            toaddrs  = 'samplereciver@mail.it"
-            msg = (IPext)
-            username = 'samplesender@email.com'
-            password = 'my_password'
-            server = smtplib.SMTP('smtp.sample.com:587')
-            server.starttls()
-            server.login(username,password)
-            server.sendmail(fromaddr, toaddrs, msg)
-            server.quit()
-            print()    
-            print("New IP has been sent!")
-        
-        else:
-            print("IP hasn't changed\r\n")
-            print(IPext)
+            if(IPext!=lastIP):
+
+                try:
+                    out_file = open("IPextLog.txt","w")
+                    out_file.write(IPext)
+                    out_file.close()
+                except:
+                    print("FILE error")
+
+                print("IP has changed\r\n")
+                print(IPext)
+                msg = (IPext)
+                try:
+                    server.starttls()
+                    server.login(username,password)
+                    server.sendmail(fromaddr, toaddrs, msg)
+                    server.quit()
+                    print()    
+                    print("New IP has been sent!")
+                except:
+                    print("MAIL error")
+            
+            else:
+                print("IP hasn't changed\r\n")
+                print(IPext)
+
+        except:
+            print("GET error")
         
         time.sleep(1800)
